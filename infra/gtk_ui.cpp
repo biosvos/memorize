2c1f2a86197864b52c24738cee65e10a54b6f8c6
jinsu <realwaterkim@datacommand.kr>
2022-10-14 08:22:58 +0900

    add 동작하도록 변경

--- infra/gtk_ui.cpp
+++ infra/gtk_ui.cpp
@@ -3,25 +3,28 @@
 //
 
 #include <gtkmm.h>
+#include <iostream>
+#include <utility>
 #include "gtk_ui.h"
 
 class AddWidget : public Gtk::Frame {
 public:
-    AddWidget() {
+    AddWidget() : button_("add") {
         Gtk::Grid grid;
         set_child(grid);
         Gtk::Label word_label("단어");
         grid.attach(word_label, 0, 0);
-        Gtk::Entry word_entry;
-        grid.attach(word_entry, 1, 0);
+        grid.attach(word_entry_, 1, 0);
 
         Gtk::Label meanings_label("뜻");
         grid.attach(meanings_label, 0, 1);
-        Gtk::Entry meanings_entry;
-        grid.attach(meanings_entry, 1, 1);
-        Gtk::Button button("button");
-        grid.attach(button, 1, 2);
+        grid.attach(meanings_entry_, 1, 1);
+        grid.attach(button_, 1, 2);
     }
+
+    Gtk::Entry word_entry_;
+    Gtk::Entry meanings_entry_;
+    Gtk::Button button_;
 };
 
 class TrainWidget : public Gtk::Frame {
@@ -59,29 +62,65 @@ public:
 
 class MainWindow : public Gtk::Window {
 public:
-    MainWindow();
+    explicit MainWindow(const std::shared_ptr<CardController> &controller) {
+        set_title("Basic application");
+        set_default_size(200, 200);
+        set_child(notebook_);
 
-private:
+        notebook_.set_expand();
+        notebook_.append_page(add_, "add");
+        notebook_.append_page(train_, "train");
+        notebook_.append_page(list_, "list");
+        notebook_.show();
+
+        SetSignals(controller);
+    }
+
+    void SetSignals(const std::shared_ptr<CardController> &controller) {
+        add_.button_.signal_clicked().connect([&]() {
+            std::cout << "asdf" << std::endl;
+            controller->Create(add_.word_entry_.get_text(), {add_.meanings_entry_.get_text()}, 0);
+        });
+    }
+
     Gtk::Notebook notebook_;
     AddWidget add_;
     TrainWidget train_;
     ListWidget list_;
 };
 
-MainWindow::MainWindow() {
-    set_title("Basic application");
-    set_default_size(200, 200);
-    set_child(notebook_);
-
-    notebook_.set_expand();
-    notebook_.append_page(add_, "add");
-    notebook_.append_page(train_, "train");
-    notebook_.append_page(list_, "list");
-    notebook_.show();
-}
-
 
 void GtkUi::Run() {
+    auto controller = factory_->CreateController(shared_from_this());
     auto app = Gtk::Application::create("org.gtkmm.examples.base");
-    app->make_window_and_run<MainWindow>(0, nullptr);
+    app->make_window_and_run<MainWindow>(0, nullptr, controller);
+}
+
+GtkUi::GtkUi(std::shared_ptr<IFactory<GtkUi>> factory) : factory_(std::move(factory)) {
+}
+
+void GtkUi::Response(const AddCardResponse &rsp) {
+    switch (rsp.result) {
+        case AddCardError::kAddCardOk:
+            std::cout << "ok" << std::endl;
+            break;
+        case AddCardError::kAddCardConflicted:
+            std::cout << "already exists" << std::endl;
+            break;
+        case AddCardError::kAddCardUnknown:
+            std::cout << "unknown error" << std::endl;
+            break;
+    }
+}
+
+void GtkUi::Response(const ListCardsResponse &rsp) {
+
+}
+
+void GtkUi::Response(const UpdateCardResponse &rsp) {
+
+}
+
+void GtkUi::Response(const PredictTrainingCasesResponse &rsp) {
+
 }
--- infra/gtk_ui.h
+++ infra/gtk_ui.h
@@ -5,10 +5,26 @@
 #ifndef MEMORIZE_GTK_UI_H
 #define MEMORIZE_GTK_UI_H
 
+#include "../adapter/card_controller.h"
 
-class GtkUi {
+class GtkUi : public std::enable_shared_from_this<GtkUi>,
+              public AddCard::IResponder, public ListCards::IResponder,
+              public UpdateCard::IResponder, public PredictTrainingCases::IResponder {
 public:
+    explicit GtkUi(std::shared_ptr<IFactory<GtkUi>> factory);
+
     void Run();
+
+    void Response(const PredictTrainingCasesResponse &rsp) override;
+
+    void Response(const UpdateCardResponse &rsp) override;
+
+    void Response(const ListCardsResponse &rsp) override;
+
+    void Response(const AddCardResponse &rsp) override;
+
+private:
+    std::shared_ptr<IFactory<GtkUi>> factory_;
 };
 
 
