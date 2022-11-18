6430de4412e66b95b231aa1ae0a8d758da82832c
jinsu <realwaterkim@datacommand.kr>
2022-11-18 15:24:24 +0900

    nana 추가 중

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include "adapter/object_box_repository.h"
 #include "adapter/forgetting_curve.h"
-#include "infra/gtk_ui.h"
 #include "usecase/usecase_impl.h"
+//#include "infra/gtk_ui.h"
+#include "infra/nana_ui.h"
 
 int main() {
     auto repository = std::make_shared<ObjectBoxRepository>();
     auto forgetting_curve = std::make_shared<ForgettingCurve>();
     auto usecase = std::make_shared<UsecaseImpl>(repository, forgetting_curve);
-    auto app = std::make_shared<GtkUi>(usecase);
+//    auto app = std::make_shared<GtkUi>(usecase);
+    auto app = std::make_shared<NanaUi>(usecase);
     app->Run();
     return 0;
 }
