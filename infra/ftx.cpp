95833793a54199d2aa3f63f60d74c2c2d2201eee
biosvos <biosvos@gmail.com>
2022-12-07 20:00:35 +0900

    ftx 추가

--- infra/ftx.cpp
+++ infra/ftx.cpp
@@ -5,9 +5,24 @@
 #include "ftx.h"
 
 #include <utility>
+#include <ftxui/dom/elements.hpp>
 
 Ftx::Ftx(std::shared_ptr<IUsecase> usecase) : usecase_(std::move(usecase)) {}
 
 void Ftx::Run() {
-
+    auto document = ftxui::hbox({
+                                        ftxui::text("left-column"),
+                                        ftxui::separator(),
+                                        ftxui::vbox({
+                                                            center(ftxui::text("top")) | ftxui::flex,
+                                                            ftxui::separator(),
+                                                            center(ftxui::text("bottom")),
+                                                    }) | ftxui::flex,
+                                        ftxui::separator(),
+                                        ftxui::text("right-column"),
+                                }) |
+            ftxui::border;
+    auto screen = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(document));
+    Render(screen, document);
+    screen.Print();
 }
--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 #include "adapter/forgetting_curve.h"
 #include "usecase/usecase_impl.h"
 //#include "infra/gtk_ui.h"
-#include "infra/nana_ui.h"
+//#include "infra/nana_ui.h"
+#include "infra/ftx.h"
 
 int main() {
     auto repository = std::make_shared<ObjectBoxRepository>();
     auto forgetting_curve = std::make_shared<ForgettingCurve>();
     auto usecase = std::make_shared<UsecaseImpl>(repository, forgetting_curve);
 //    auto app = std::make_shared<GtkUi>(usecase);
-    auto app = std::make_shared<NanaUi>(usecase);
+//    auto app = std::make_shared<NanaUi>(usecase);
+    auto app = std::make_shared<Ftx>(usecase);
     app->Run();
     return 0;
 }
