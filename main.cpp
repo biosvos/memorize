402c1351ddfe77ea17b7938db1421ff6dca034a6
biosvos <biosvos@gmail.com>
2022-12-07 20:32:35 +0900

    objecbox 버전 내림, 왜인지 올리면 맛이 감

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include "adapter/memory_repository.h"
+//#include "adapter/memory_repository.h"
+#include "adapter/object_box_repository.h"
 #include "adapter/forgetting_curve.h"
 #include "usecase/usecase_impl.h"
 #include "infra/ftx.h"
 
 int main() {
-    auto repository = std::make_shared<MemoryRepository>();
+//    auto repository = std::make_shared<MemoryRepository>();
+    auto repository = std::make_shared<ObjectBoxRepository>();
     auto forgetting_curve = std::make_shared<ForgettingCurve>();
     auto usecase = std::make_shared<UsecaseImpl>(repository, forgetting_curve);
 //    auto app = std::make_shared<GtkUi>(usecase);
