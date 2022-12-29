48d36bde4ec8217a72ff14275ab28d4fc42baf2c
jinsu <realwaterkim@datacommand.kr>
2022-12-29 13:24:18 +0900

    add ui command

--- main.cpp
+++ main.cpp
@@ -20,12 +20,27 @@ public:
     }
 };
 
+class UiOptions : public argumentum::CommandOptions {
+public:
+    explicit UiOptions(const std::string_view &name) :
+            CommandOptions(name) {}
+
+    void execute(const argumentum::ParseResult &result) override {
+        auto repository = std::make_shared<ObjectBoxRepository>();
+        auto forgetting_curve = std::make_shared<ForgettingCurve>();
+        auto usecase = std::make_shared<UsecaseImpl>(repository, forgetting_curve);
+        auto app = std::make_shared<Ui>(usecase);
+        app->Run();
+    }
+};
+
 int main(int argc, char **argv) {
     auto parser = argumentum::argument_parser{};
     auto params = parser.params();
 
     parser.config().program(argv[0]).description("memorize words");
     params.add_command<CliOptions>("cli").help("cli");
+    params.add_command<UiOptions>("ui").help("ui");
 
     const argumentum::ParseResult &result = parser.parse_args(argc, argv, 1);
     if (!result) {
